31bddf5528cc5f277b1a79275819cd89d392a397
mauro bono <bono.mauro@gmail.com>
2020-02-16 20:39:04 +0100

    [START] First Commit and Project Setup
    Basic setup with Gatsbyjs and its default theme. Removed anything not necessary now, even layout.
    
    Added all useful packages for the project:
    	- styled components;
    	- netlify cms;
    	- react icons;
    	- google analytics;
    	- google maps;
    	- react share;
    	- react slick;
    	- react spring;
    	- react visibility sensor;
    	- gatsby plugin sitemap;
    	- cookie consent

---
+++ README.md
@@ -0,0 +1,1 @@
+# Museo Civico di Nepi
---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `Museo Civico di Nepi`,
+    description: `Sito Web del Museo Civico del Comune di Nepi`,
+    author: `@uptheirons78`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `museo-civico-nepi`,
+        short_name: `museo-nepi`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
