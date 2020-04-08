8b2d92a12978f886f3ce7da97f9feeb65b7524bc
mauro bono <bono.mauro@gmail.com>
2020-04-08 18:57:07 +0200

    [ADD] 404 page

---
+++ src/components/404/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "gatsby";
+import Img from "gatsby-image";
+
+// Components
+import HeadingSection from "../Shared/HeadingSection";
+import {
+  SinglePageWrapper,
+  PageDescription,
+  Main,
+  DoubleGrid,
+} from "../Styles/StyledPageElement";
+
+const FourOFour = ({ data, description }) => {
+  return (
+    <>
+      <HeadingSection thumbnail={data.banner.childImageSharp.fixed.src}>
+        <h2>404: Pagina Non Trovata</h2>
+      </HeadingSection>
+      <SinglePageWrapper>
+        <PageDescription>{description}</PageDescription>
+        <Main>
+          <DoubleGrid top="3rem" bottom="3rem">
+            <div className="left-block text-block">
+              <div>
+                <h4>Ecco alcune pagine che potrebbero interessarti</h4>
+                <div>
+                  <p>
+                    Qualora volessi prendere visione di cosa offre il nostro
+                    museo o la città di Nepi:
+                  </p>
+                  <ul>
+                    <Link to="/museo">Museo</Link>
+                    <br />
+                    <Link to="/monumenti">Monumenti</Link>
+                    <br />
+                    <Link to="/eventi">Eventi</Link>
+                  </ul>
+                </div>
+                <div style={{ marginTop: "3rem" }}>
+                  <p>
+                    Altrimenti puoi sempre far ritorno alla nostra home page o
+                    contattarci per qualsiasi tua necessità:
+                  </p>
+                  <ul>
+                    <Link to="/">Home</Link>
+                    <br />
+                    <Link to="/contatti">Contatti</Link>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="right-block">
+              <Img fluid={data.firstPicture.childImageSharp.fluid} />
+            </div>
+          </DoubleGrid>
+        </Main>
+      </SinglePageWrapper>
+    </>
+  );
+};
+
+export default FourOFour;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { graphql } from "gatsby";
+//Components
+import Layout from "../components/Layout/layout";
 import SEO from "../components/seo";
+import FourOFour from "../components/404";
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-);
+const NotFoundPage = ({ data }) => {
+  const description =
+    "Gentile Visitatore la pagina che stai cercando non esiste all'interno del sito del Museo Civico di Nepi, è stata, probabilmente, rimossa o risulta al momento inaccessibile per problemi tecnici. Ci scusiamo per il disagio.";
+  return (
+    <Layout language="it">
+      <SEO
+        lang="it"
+        title="404: Pagina Non Trovata"
+        description={description}
+      />
+      <FourOFour data={data} description={description} />
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
+
+export const query = graphql`
+  query {
+    firstPicture: file(relativePath: { eq: "museo/interni-museo-01.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 400) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    banner: file(relativePath: { eq: "banners/museo-banner.jpg" }) {
+      childImageSharp {
+        fixed(width: 1100, height: 300) {
+          src
+        }
+      }
+    }
+  }
+`;
