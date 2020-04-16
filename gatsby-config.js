2286aa9bf644e628ad2dbddb522062caa0954f09
mauro bono <bono.mauro@gmail.com>
2020-04-16 22:10:10 +0200

    [UPDATE] Netlify CMS with Cloudinary Media Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,12 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
