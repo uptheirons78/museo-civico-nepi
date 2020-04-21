3de21a31ed764e0f3e3b2cd9e40b9911e540c4b4
mauro bono <bono.mauro@gmail.com>
2020-04-21 13:17:10 +0200

    🐛 FIX:  Caching problems with gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,5 +92,27 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/public/**/*.html": [
+            "cache-control: public",
+            "cache-control:  max-age=0",
+            "cache-control: must-revalidate",
+          ],
+          "/public/sw.js": [
+            "cache-control: public",
+            "cache-control:  max-age=0",
+            "cache-control: must-revalidate",
+          ],
+          "/public/page-data/*": [
+            "cache-control: public",
+            "cache-control:  max-age=0",
+            "cache-control: must-revalidate",
+          ],
+        },
+      },
+    },
   ],
 };
